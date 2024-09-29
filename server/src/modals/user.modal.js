2ae12ddeb7be36c2029190bde511db8538542401
Anshul Sharma <sharma.ansh2926@gmail.com>
2024-09-29 15:17:56 +0530

    Added multer middleware and user methods

--- server/src/modals/user.modal.js
+++ server/src/modals/user.modal.js
@@ -1,7 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 import bcryptjs from "bcryptjs";
-
-
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
 const userSchema = new Schema({
     username : {
@@ -69,6 +69,50 @@ userSchema.pre('save', async function (next) {
     this.password = await bcryptjs.hash(this.password, 10);
     next();
 })
+
+
+userSchema.methods = {
+    generateAccessToken : function(){
+        return jwt.sign(
+            {
+                _id : this._id,
+                email : this.email,
+                username : this.username
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            {
+                expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+            }
+        )
+    },
+
+    generateRefreshToken : function(){
+        return jwt.sign(
+            {
+                _id : this._id,
+            },
+            process.env.REFRESH_TOKEN_SECRET,
+            {
+                expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+            }
+        )
+    },
+
+    isPasswordCorrect : async function (password) {
+        if(!password || typeof password !== 'string'){
+            throw new Error("Password must be a string");
+        }
+
+        return await bcryptjs.compare(password, this.password);
+    },
+
+    generatePasswordResetToken : async function() {
+        const resetToken = crypto.randomBytes(20).toString('hex');
+        this.forgotPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+        this.forgotPasswordExpiry = Date.now() + 15 * 60 * 1000;
+        return resetToken;
+    }
+}
 
 
 
--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { getProfile, login, logout, register } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 
 const router = Router();
 
 
-router.route("/register").post(register);
+router.route("/register").post( upload.single("avatar"), register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
 router.route("/me").get(getProfile);
