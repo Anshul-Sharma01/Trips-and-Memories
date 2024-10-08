3f46bb581bb3cda2e5bfb4ed098751429e611cd5
Anshul Sharma <sharma.ansh2926@gmail.com>
2024-10-08 18:40:24 +0530

    Implemented logic for accepting,declining and fetching all friend requests

--- server/src/controllers/friendship.controller.js
+++ server/src/controllers/friendship.controller.js
@@ -84,16 +84,91 @@ const cancelFriendRequest = asyncHandler(async(req, res, next) => {
 })
 
 const acceptFriendRequest = asyncHandler(async(req, res, next) => {
+    try{
+        const { requestId } = req.params;
+        const userId = req.user._id;
 
+        const friendRequest = await Friendship.findById(requestId);
+
+        if(!friendRequest || friendRequest.recipient.toString() !== userId.toString()){
+            throw new ApiError(404, "Friend Request not found or you are not the recipient of the request !!");
+        }
+
+        friendRequest.status = 'accepted';
+        await friendRequest.save();
+
+        return res.status(200)
+        .json(
+            new ApiResponse(
+                200,
+                friendRequest,
+                "Friend Request Accepted Successfully"
+            )
+        );
+
+    }catch(err){
+        console.error(`Error occurred while accepting the friend Request : ${err}`);
+        throw new ApiError(400, err?.message || "Error occurred while accepting the friend request !!");
+    }
 })
 
 
 const declineFriendRequest = asyncHandler(async(req, res, next) => {
+    try{
+        const { requestId } = req.params;
+        const userId = req.user._id;
+        if(!isValidObjectId(requestId)){
+            throw new ApiError(400, "Invalid Request Id");
+        }
 
+        const friendRequest = await Friendship.findById(requestId);
+
+        if(!friendRequest || friendRequest.recipient.toString() !== userId.toString()){
+            throw new ApiError(404, "Friend request not found or you are not the recipient");
+        }
+
+        friendRequest.status = "declined";
+        await friendRequest.save();
+
+        return res.status(200)
+        .json(
+            new ApiResponse(
+                200,
+                friendRequest,
+                "Friend Request Declined Successfully"
+            )
+        )
+
+    }catch(err){
+        console.error(`Error occurred while declining the friend request : ${err}`);
+        throw new ApiError(400, err?.message || "Error occurred while decling the friend request !!");
+    }
 })
 
 const listAllFriends = asyncHandler(async(req, res, next) => {
+    try{
+        const userId = req.user._id;
 
+        const acceptedFriends = await Friendship.find({
+            $or : [
+                { requester : userId}, { recipient : userId }
+            ],
+            status : 'accepted'
+        }).populate('request recipient', 'name email');
+
+        return res.status(200)
+        .json(
+            new ApiResponse(
+                200,
+                acceptedFriends,
+                "Successfully fetched friends list"
+            )
+        )
+        
+    }catch(err){
+        console.error(`Error occurred while fetching the friend list : ${err}`);
+        throw new ApiError(400, err?.message || "Error occurred while fetching the friend list");
+    }
 })
 
 export {
