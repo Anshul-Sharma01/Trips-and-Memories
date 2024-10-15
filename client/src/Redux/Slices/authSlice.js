d0a3ec769ecd1ae2a5d7f2002ee4d3ba1244db89
Anshul Sharma <sharma.ansh2926@gmail.com>
2024-10-15 21:25:35 +0530

    updated authSlice to increase modularity

--- client/src/Redux/Slices/authSlice.js
+++ client/src/Redux/Slices/authSlice.js
@@ -1,141 +1,129 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axiosInstance from "../../Helpers/axiosInstance.js";
 import toast from "react-hot-toast";
-import axios from "axios";
 
 
-
-const initialState = {
-    isLoggedIn : localStorage.getItem("isLoggedIn") === "true",
-    userRole : localStorage.getItem("userRole") !== undefined ? localStorage.getItem("userRole") : " ",
-    userData : JSON.parse(localStorage.getItem("userData")) !== undefined ? JSON.parse(localStorage.getItem("userData")) : {},
+const updateLocalStorage = (user) => {
+    localStorage.setItem("userData", JSON.stringify(user));
+    localStorage.setItem("isLoggedIn", "true");
+    localStorage.setItem("userRole", user?.role);
 };
 
 
-export const createUserAccount = createAsyncThunk("/auth/register", async(data) => {
-    try{
+const toastHandler = (promise, loadingMsg, successMsg, errorMsg) => {
+    return toast.promise(promise, {
+        loading: loadingMsg,
+        success: (data) => data?.data?.message || successMsg,
+        error: errorMsg,
+    });
+};
+
+const initialState = {
+    isLoggedIn: localStorage.getItem("isLoggedIn") === "true",
+    userRole: localStorage.getItem("userRole") !== undefined ? localStorage.getItem("userRole") : "",
+    userData: JSON.parse(localStorage.getItem("userData")) !== undefined ? JSON.parse(localStorage.getItem("userData")) : {},
+};
+
+
+export const createUserAccount = createAsyncThunk("/auth/register", async (data) => {
+    try {
         const res = axiosInstance.post("users/register", data);
-        toast.promise(res, {
-            loading : " Creating your account..",
-            success : (data) => data?.data?.message,
-            error : "Failed to create a new account !!"
-        });
-
+        toastHandler(res, "Creating your account..", "Account created successfully!", "Failed to create a new account!");
         return (await res).data;
-
-    }catch(err){
-        console.error(`Error occurred in creating new account : ${err}`);
+    } catch (err) {
+        console.error(`Error occurred in creating new account: ${err}`);
     }
-})
+});
 
-export const authenticateUser = createAsyncThunk("/auth/login", async(data) => {
-    try{
+export const authenticateUser = createAsyncThunk("/auth/login", async (data, { rejectWithValue }) => {
+    try {
         const res = axiosInstance.post("users/login", data);
-        toast.promise(res, {
-            loading : "Authenticating your credentials...",
-            success : (data) => data?.data?.message,
-            error : "Failed to authenticate the credentials !!"
-        })
+        toastHandler(res, "Authenticating your credentials...", "Login successful!", "Failed to authenticate the credentials!");
         return (await res).data;
-    }catch(err){
-        console.error(`Error occurred while authenticating the user : ${err}`);
+    } catch (err) {
+        console.error(`Error occurred while authenticating the user: ${err}`);
+        return rejectWithValue(err.response.data);
     }
-})
+});
 
-export const logoutUserAccount = createAsyncThunk("/auth/logout", async() => {
-    try{
+export const logoutUserAccount = createAsyncThunk("/auth/logout", async () => {
+    try {
         const res = axiosInstance.get("users/logout");
-        toast.promise(res, {
-            loading : "Logging out...",
-            success : (data) => data?.data?.message,
-            error : "Failed to log out !!"
-        })
-
+        toastHandler(res, "Logging out...", "Successfully logged out!", "Failed to log out!");
         return (await res).data;
-
-    }catch(err){
-        console.error(`Error occurred while logging out the user : ${err}`);
+    } catch (err) {
+        console.error(`Error occurred while logging out the user: ${err}`);
     }
-})
+});
 
-export const forgotPasswordThunk = createAsyncThunk("/auth/forgot-password", async(data) => {
-    try{
+export const forgotPasswordThunk = createAsyncThunk("/auth/forgot-password", async (data) => {
+    try {
         const res = axiosInstance.post("users/reset", data);
-        toast.promise(res, {
-            loading : 'sending reset token..',
-            success : (data) => data?.data?.message,
-            error : "Email not registered or an error occurred !!"
-        })
+        toastHandler(res, "Sending reset token...", "Reset token sent!", "Email not registered or an error occurred!");
         return (await res).data;
-    }catch(err){
-        console.error(`Error occurred while forgetting password : ${err}`);
+    } catch (err) {
+        console.error(`Error occurred while forgetting password: ${err}`);
     }
-})
+});
 
-export const resetPasswordThunk = createAsyncThunk("/auth/reset-password", async(data) => {
-    try{
-        const res = axiosInstance.post(`users/reset/${data.resetToken}`, {password : data.password});
-        toast.promise(res, {
-            loading : "Updating your password",
-            success : (data) => data?.data?.message,
-            error : "Failed to update the password"
-        })
+export const resetPasswordThunk = createAsyncThunk("/auth/reset-password", async (data) => {
+    try {
+        const res = axiosInstance.post(`users/reset/${data.resetToken}`, { password: data.password });
+        toastHandler(res, "Updating your password...", "Password updated successfully!", "Failed to update the password!");
         return (await res).data;
-    }catch(err){
-        console.error(`Error occurred while resetting the password : ${err}`);
+    } catch (err) {
+        console.error(`Error occurred while resetting the password: ${err}`);
     }
-})
+});
+
 
 const authSlice = createSlice({
-    name : 'auth',
+    name: 'auth',
     initialState,
-    reducers : {},
-    extraReducers : (builder) => {
-        builder.addCase(createUserAccount.fulfilled, (state, action) => {
-            if(action?.payload?.statusCode === 201){
-                const user = action?.payload?.data?.user;
-                localStorage.setItem("userData", JSON.stringify(user));
-                localStorage.setItem("isLoggedIn", true);
-                localStorage.setItem("userRole", user?.role);
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(createUserAccount.fulfilled, (state, action) => {
+                if (action?.payload?.statusCode === 201) {
+                    const user = action?.payload?.data?.user;
+                    updateLocalStorage(user);
 
-                state.isLoggedIn = true;
-                state.userData = user;
-                state.userRole = user?.role;
-            }
-        })
-        .addCase(createUserAccount.rejected, (state, action) => {
-            localStorage.clear();
-            state.userData = {};
-            state.isLoggedIn = false;
-            state.userRole = "";
-        })
-        .addCase(authenticateUser.fulfilled, (state, action) => {
-            if(action?.payload?.statusCode === 200){
-                const user = action?.payload?.data?.user;
-                localStorage.setItem("userData", JSON.stringify(user));
-                localStorage.setItem("isLoggedIn", true);
-                localStorage.setItem("userRole", user?.role);
-
-                state.isLoggedIn = true;
-                state.userData = user;
-                state.userRole = user?.role;
-            }
-        })
-        .addCase(authenticateUser.rejected, (state) => {
-            localStorage.clear();
-            state.userData = {},
-            state.isLoggedIn = false;
-            state.userRole = "";
-        })
-        .addCase(logoutUserAccount.fulfilled, (state, action) => {
-            if (action?.payload?.statusCode === 200) {
+                    state.isLoggedIn = true;
+                    state.userData = user;
+                    state.userRole = user?.role;
+                }
+            })
+            .addCase(createUserAccount.rejected, (state) => {
                 localStorage.clear();
-                state.isLoggedIn = false;
                 state.userData = {};
+                state.isLoggedIn = false;
                 state.userRole = "";
-            }
-        })
+            })
+            .addCase(authenticateUser.fulfilled, (state, action) => {
+                if (action?.payload?.statusCode === 200) {
+                    const user = action?.payload?.data?.user;
+                    updateLocalStorage(user);
+
+                    state.isLoggedIn = true;
+                    state.userData = user;
+                    state.userRole = user?.role;
+                }
+            })
+            .addCase(authenticateUser.rejected, (state) => {
+                localStorage.clear();
+                state.userData = {};
+                state.isLoggedIn = false;
+                state.userRole = "";
+            })
+            .addCase(logoutUserAccount.fulfilled, (state, action) => {
+                if (action?.payload?.statusCode === 200) {
+                    localStorage.clear();
+                    state.isLoggedIn = false;
+                    state.userData = {};
+                    state.userRole = "";
+                }
+            });
     }
-})
+});
 
 export default authSlice.reducer;
