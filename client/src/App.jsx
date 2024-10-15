d2c2ad1f6c0129c3608af7774488fea30c0f7a24
Anshul Sharma <sharma.ansh2926@gmail.com>
2024-10-15 19:14:54 +0530

    Added a 404 not found page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,27 @@
-import { useState } from 'react'
-import LandingPage from "./Pages/LandingPage.jsx";
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import LandingPage from "./Pages/LandingPage.jsx";
 import Register from './Pages/Register.jsx';
 import Login from './Pages/Login.jsx';
 import ForgotPassword from './Pages/ForgotPassword.jsx';
 import ResetPassword from './Pages/ResetPassword.jsx';
-
+import NotFound from './Pages/NotFound.jsx';
 
 function App() {
+
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<LandingPage/>} ></Route>
-        <Route path="/auth/register" element={<Register/>} ></Route>
-        <Route path='/auth/login' element={<Login/>}></Route>
-        <Route path='/auth/forgot-password' element={<ForgotPassword />} ></Route>
-        <Route path='/auth/reset/:resetToken' element={<ResetPassword/>}></Route>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/auth/register" element={<Register />} />
+        <Route path='/auth/login' element={<Login />} />
+        <Route path='/auth/forgot-password' element={<ForgotPassword />} />
+        <Route path='/auth/reset/:resetToken' element={<ResetPassword />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
