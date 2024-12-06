6de665708c94fae335d52327dcce4637e1d4239d
Anshul Sharma <sharma.ansh2926@gmail.com>
2024-12-06 18:15:19 +0530

    Added more security for protected routes using RequireAuth component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage.jsx";
 import Register from './Pages/Register.jsx';
@@ -14,32 +13,20 @@ import PersonalMemories from "./Pages/Memory/PersonalMemories.jsx";
 import AllMemories from "./Pages/Memory/AllMemories.jsx";
 import ViewMemory from "./Pages/Memory/ViewMemory.jsx";
 
-import UpdateMemoryThumbnail from "./Components/Memory/UpdateMemoryThumbnail.jsx";
-import UpdateMemoryDetails from "./Components/Memory/UpdateMemoryDetails.jsx";
-import DeleteMemoryModal from "./Components/Memory/DeleteMemoryModal.jsx";
-import ToggleBucketList from "./Components/BucketList/ToggleBucketList.jsx";
 import BucketList from "./Pages/BucketList/BucketList.jsx";
-import Comment from "./Components/Comments/Comment.jsx";
-import CommentsSection from "./Components/Comments/CommentsSection.jsx";
-import DeleteComment from "./Components/Comments/DeleteComment.jsx";
-import ToggleLike from "./Components/Like/ToggleLike.jsx";
-import Friend from "./Components/Friends/Friend.jsx";
 import FriendProfile from "./Pages/Friends/FriendProfile.jsx";
 import AllFriends from "./Pages/Friends/AllFriends.jsx";
-import SearchFriend from "./Components/Friends/SearchFriend.jsx";
-import PendingRequests from "./Components/Friends/PendingRequests.jsx";
 import TripsRecomm from "./Pages/Recommendations/TripsRecomm.jsx";
 import CreateTimeCapsule from "./Pages/TimeCapsule/CreateTimeCapsule.jsx";
-import TimeCapsule from "./Components/TimeCapsule/TimeCapsule.jsx";
 import PersonalTimeCapsules from "./Pages/TimeCapsule/PersonalTimeCapsules.jsx";
 import ViewTimeCapsule from "./Pages/TimeCapsule/ViewTimeCapsule.jsx";
 import CreateJournal from "./Pages/journal/CreateJournal.jsx";
-import TripJournal from "./Components/journal/TripJournal.jsx";
 import MyJournals from "./Pages/journal/MyJournals.jsx";
 import AddEntry from "./Pages/journal/AddEntry.jsx";
-import JournalEntry from "./Components/journal/JournalEntry.jsx";
 import ViewJournal from "./Pages/journal/ViewJournal.jsx";
 import ManageContributors from "./Components/journal/ManageContributors.jsx";
+
+import RequireAuth from "./Helpers/RequireAuth.jsx";
 
 
 function App() {
@@ -55,7 +42,6 @@ function App() {
         <Route path='/auth/forgot-password' element={<ForgotPassword />} />
         <Route path='/auth/reset/:resetToken' element={<ResetPassword />} />
 
-        <Route path="/get-recomm" element={<TripsRecomm/>}></Route>
 
         <Route path="/user/me/profile" element={<Profile/>}></Route>
         <Route path="users/friends-list" element={<AllFriends/>}></Route>
@@ -64,22 +50,29 @@ function App() {
         <Route path="/denied" element={<Denied />} > </Route>
         <Route path="/auth/change-password" element={<ChangePassword/>} ></Route>
 
-        <Route path="/time-capsules/create" element={<CreateTimeCapsule/>}></Route>
-        <Route path="/time-capsules/fetch/my" element={<PersonalTimeCapsules/>}></Route>
-        <Route path="/time-capsules/view/:capsuleId" element={<ViewTimeCapsule/>}></Route>
+        
 
-        <Route path="/create-journal" element={<CreateJournal/>}></Route> 
-        <Route path="/my-journals" element={<MyJournals/>}></Route>
-        <Route path="/add-entry/:journalId" element={<AddEntry/>}></Route>
-        <Route path="/view-journal/:journalId" element={<ViewJournal/>}></Route>
-        <Route path="/manage-contributors/:journalId" element={<ManageContributors/>}></Route>
+
+        <Route element={<RequireAuth allowedRoles={['USER']} />}>
+            <Route path="/get-recomm" element={<TripsRecomm/>}></Route>
+            <Route path="/time-capsules/create" element={<CreateTimeCapsule/>}></Route>
+            <Route path="/time-capsules/fetch/my" element={<PersonalTimeCapsules/>}></Route>
+            <Route path="/time-capsules/view/:capsuleId" element={<ViewTimeCapsule/>}></Route>
+            <Route path="/create-journal" element={<CreateJournal/>}></Route> 
+            <Route path="/my-journals" element={<MyJournals/>}></Route>
+            <Route path="/add-entry/:journalId" element={<AddEntry/>}></Route>
+            <Route path="/view-journal/:journalId" element={<ViewJournal/>}></Route>
+            <Route path="/manage-contributors/:journalId" element={<ManageContributors/>}></Route>
+        </Route>
 
         <Route path="/memory/all" element={<AllMemories/>}></Route>
-        <Route path="/memory/create-memory" element={<CreateMemory/>} ></Route>
-        <Route path="/memory/my" element={<PersonalMemories/>}></Route>
-        <Route path="/memory/:memoryId" element={<ViewMemory/>}></Route>
 
-        <Route path="/my-bucket-list" element={<BucketList/>}></Route>
+        <Route element={<RequireAuth allowedRoles={['USER']}/>}>
+            <Route path="/memory/create-memory" element={<CreateMemory/>} ></Route>
+            <Route path="/memory/my" element={<PersonalMemories/>}></Route>
+            <Route path="/memory/:memoryId" element={<ViewMemory/>}></Route>
+            <Route path="/my-bucket-list" element={<BucketList/>}></Route>
+        </Route>
 
         <Route path='*' element={<NotFound />} />
 
