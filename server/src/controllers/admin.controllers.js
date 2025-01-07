6b86211c7b748fc29ca28f4976d9cfd1352db725
Anshul Sharma <sharma.ansh2926@gmail.com>
2025-01-07 20:44:57 +0530

    Implement user, memory, comment, and like deletion functionality with appropriate error handling

--- server/src/controllers/admin.controllers.js
+++ server/src/controllers/admin.controllers.js
@@ -1,12 +1,14 @@
 
 import { User } from "../models/user.model.js";
 import { Memory } from "../models/memory.model.js";
+import { Friendship } from "../models/friendship.model.js";
 import { Comment } from "../models/comment.model.js";
 import { Like } from "../models/like.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { isValidObjectId } from "mongoose";
+import { deleteFromCloudinary } from "../utils/cloudinary.js";
 
 
 const fetchAllUsers = asyncHandler(async(req, res, next) => {
@@ -34,7 +36,7 @@ const fetchUsersCount = asyncHandler(async(req, res, next) => {
     try{
         
         const allUsers = await User.find({}).countDocuments();
-        if(allUsers.length == 0){
+        if(allUsers == 0){
             return res.status(200)
             .json(
                 new ApiResponse(
@@ -96,6 +98,44 @@ const updateUserById = asyncHandler(async(req, res, next) => {
 
 const deleteUserById = asyncHandler(async(req, res, next) => {  
     try{
+        const { userId } = req.params;
+        const { deleteMemories, removeLikes, removeComments } = req.query;
+        if(!isValidObjectId(userId)){
+            throw new ApiError(400, "Invalid User Id");
+        }
+
+        const userInfo = await User.deleteOne();
+        if(!userInfo){
+            throw new ApiError(400, "User does not exists");
+        }
+
+        const friendShipInfo = await Friendship.deleteMany(
+            { $or: [{ requester: userId }, { recipient: userId }] }
+        ); 
+
+        
+
+        if(deleteMemories){
+            const memoryInfo = await Memory.deleteMany({ author : userId });
+        }
+        if(removeLikes){
+            const likesInfo = await Like.deleteMany({ likedBy : userId });
+        }
+        if(removeComments){
+            const commentInfo = await Comment.deleteMany({ owner : userId });
+        }
+
+
+
+
+        return res.status(200)
+        .json(
+            new ApiResponse(
+                200,
+                userInfo,
+                "User Data successfully wiped off"
+            )
+        );
 
     }catch(err){
         console.error(`Error occurred while deleting user by id : ${err}`);
@@ -106,7 +146,7 @@ const fetchMemoriesCount = asyncHandler(async(req, res, next) => {
     try{
 
         const allMemories = await Memory.find({}).countDocuments();
-        if(allMemories.length == 0){
+        if(allMemories == 0){
             return res.status(200)
             .json(
                 new ApiResponse(
@@ -191,6 +231,28 @@ const updateMemoryById = asyncHandler(async(req, res, next) => {
 const deleteMemoryById = asyncHandler(async(req, res, next) => {    
     try{
 
+        const { memoryId } = req.params;
+        if(!isValidObjectId(memoryId)){
+            throw new ApiError(400, "Invalid Memory Id");
+        }
+
+        const memoryInfo = await Memory.findByIdAndDelete(memoryId);
+        
+        if(!memoryInfo){
+            throw new ApiError(404, "Memory does not exists");
+        }
+
+        await deleteFromCloudinary(memoryInfo.thumbnail.public_id);
+
+        return res.status(200)
+        .json(
+            new ApiResponse(
+                200,
+                memoryInfo,
+                "Memory successfully deleted"
+            )
+        );
+
     }catch(err){
         console.error(`Error occurred while deleting memory by id : ${err}`);
     }
@@ -200,7 +262,7 @@ const fetchCommentsCount = asyncHandler(async(req, res, next) => {
     try{
 
         const allComments = await Comment.find({}).countDocuments();
-        if(allComments.length == 0){
+        if(allComments == 0){
             return res.status(200)
             .json(
                 new ApiResponse(
@@ -285,6 +347,26 @@ const updateCommentById = asyncHandler(async(req, res, next) => {
 const deleteCommentById = asyncHandler(async(req, res, next) => {  
     try{
 
+        const { commentId } = req.params;
+        if(!isValidObjectId(commentId)){
+            throw new ApiError(400, "Invalid Comment Id");
+        }
+
+        const commentInfo = await Comment.findByIdAndDelete(commentId);
+        if(!commentInfo){
+            throw new ApiError(404, "Comment does not exists");
+        }
+
+        return res.status(200)
+        .json(
+            new ApiResponse(
+                200,
+                commentInfo,
+                "Comment successfully deleted"
+            )
+        );
+
+
     }catch(err){
         console.error(`Error occurred while deleting comment by id : ${err}`);
     }
@@ -294,7 +376,7 @@ const fetchLikesCount = asyncHandler(async(req, res, next) => {
     try{
 
         const allLikes = await Like.find({}).countDocuments();
-        if(allLikes.length == 0){
+        if(allLikes == 0){
             return res.status(200)
             .json(
                 new ApiResponse(
@@ -377,7 +459,27 @@ const updateLikeById = asyncHandler(async(req, res, next) => {
 })  
 
 const deleteLikeById = asyncHandler(async(req, res, next) => {
-    try{
+    try{    
+
+        const { likeId } = req.params;
+        if(!isValidObjectId(likeId)){
+            throw new ApiError(400, "Invalid Like Id");
+        }
+
+        const likeInfo = await Like.findByIdAndDelete(likeId);
+        if(!likeInfo){
+            throw new ApiError(404, "Like does not exists");
+        }
+
+        return res.status(200)
+        .json(
+            new ApiResponse(
+                200,
+                likeInfo,
+                "Like successfully deleted"
+            )
+        );
+        
 
     }catch(err){
         console.error(`Error occurred while deleting like by id : ${err}`);
